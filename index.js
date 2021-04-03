b11404b83912aef8e18f6550ae4da0bf2cd50b7f
Even Stensberg <evenstensberg@gmail.com>
2021-04-03 17:14:02 +0200

    chore: cleanup

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { gray, bold, white, cyan, yellow } = require('colorette');
+const { gray, bold, white } = require('colorette');
 const ansiEscapes = require('ansi-escapes');
 const readline = require('readline');
 
@@ -24,88 +24,6 @@ const setupInteractive = () => {
     console.clear();
     console.log(usagePrompt);
 };
-
-const informActions = () => {
-    console.log('You can now analyze your build, press c to continue...\n');
-};
-
-/* const writeFilterConsole = () => {
-    if (state.length) {
-        const latestCompilation = state[state.length - 1];
-        const data = [];
-
-        for (let i = 0; i < latestCompilation.chunks.length; i++) {
-            const name = latestCompilation.chunks[i].id;
-            const chunksArr = [];
-            for (let j = 0; j < latestCompilation.chunks[i].modules.length; j++) {
-                const size = latestCompilation.chunks[i].modules[j].size;
-                const path = latestCompilation.chunks[i].modules[j].name.replace('./', '');
-                const issuerPath = latestCompilation.chunks[i].modules[j].issuerPath;
-                chunksArr.push({ path, size, issuerPath });
-            }
-            data.push({ [name]: chunksArr });
-        }
-        console.clear();
-        data.forEach((chunk) => {
-            Object.keys(chunk).forEach((mod) => {
-                console.log(bold(cyan(mod)));
-                chunk[mod].forEach((sub) => {
-                    console.log('> ', yellow(sub.path));
-                });
-            });
-        });
-        process.stdout.write(ansiEscapes.cursorTo(0, 1));
-    }
-}; */
-
-
-const EXIT_KEY = 'q';
-const ANALYZE_KEY = 'a';
-const FILTER_KEY = 'm';
-const ENTER_KEY = '\n';
-const PAUSE_KEY = 'p';
-
-const state = [];
-const interactiveConfig = [
-    {
-        key: ANALYZE_KEY,
-        description: 'Analyze build for performance improvements',
-        onShowMore: {
-            action: () => {},
-        },
-    },
-    {
-        key: PAUSE_KEY,
-        description: 'Pause compilation at a given time',
-        onShowMore: {
-            action: () => {},
-        },
-    },
-    {
-        key: FILTER_KEY,
-        description: 'Filter a module and get stats on why a module was included',
-        onShowMore: {
-            action: () => {},
-        },
-    },
-    {
-        key: ENTER_KEY,
-        description: 'Run webpack',
-        onShowMore: {
-            action: () => {},
-        },
-    },
-    {
-        key: EXIT_KEY,
-        description: 'Exit interactive mode',
-        onShowMore: {
-            action: () => {
-                console.clear();
-                process.exit();
-            }
-        },
-    },
-];
 
 modules.exports = async function run(config) {
     const stdin = process.stdin;
